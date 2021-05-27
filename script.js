074cc9832c28c74dbbfcffff6d95e8f5ac89eb6b
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2021-05-27 13:52:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
-var first = document.getElementById("first");
-var container = document.getElementById("container");
+// event.target.(value) - > When I click a button, this object return the button index.
+// event.buttons - > When I click a button, this object return what button I clicked on the mouse.
+
+var gameBoard = document.getElementById("gameBoard");
 var arrBtn = [], frq = [];
 var open = true, bombs = -1;
 
-function startGame() {
-            first.remove();
-            document.getElementById('title').innerHTML = "&#128515;";
-            return createBoard();
+
+window.addEventListener('DOMContentLoaded', function() { 
+        for(var i = 1; i <= 81; ++i) {
+                $(gameBoard).append(`<button class = "btn" id = "`+ i +`" onmousedown = "mouseEvent(event);"></button`);           
+                arrBtn.push(1);
+                                           
+                if(i % 9 == 0) {
+                        $(gameBoard).append("<br>");
+                }
+        }
+});
+
+function mouseEvent(event) {
+        var btnTrigg = event.target.id - 1, mouseClick = event.buttons;
+             
+        if(mouseClick == 1) {
+                return makeSteps(btnTrigg);
+        }
+        return placeFlag(btnTrigg);
 }
 
-function createBoard() {
-            for(var i = 1; i <= 81; ++i) {
-                    var button = document.createElement('button');
-                    button.type = 'button';
-                    button.className = 'btn';
-                    button.id = i;
-                            
-                    arrBtn.push(1);
-                    container.appendChild(button);
 
-                    button.onmousedown = function(event) {
-                        var btnTrigg = event.target.id - 1, mouseClick = event.buttons;
-                                    
-                        if(mouseClick == 1) {
-                                return makeSteps(btnTrigg);
-                        }
-                        return placeFlag(btnTrigg);
-                    }
-                            
-                    if(i % 9 == 0) {
-                            var space = document.createElement("br");
-                            container.appendChild(space);
-                    }
-            }
-
-}
 
 function makeSteps(btnTrigg) {
-            if(frq[btnTrigg] == 2)
-                    return placeFlags(btnTrigg);            
-            if(arrBtn[btnTrigg] % 4 == 0) 
-                    return detonateBombs();
-            if(bombs == 0)
-                    return winnerSweeper();            
+        if(frq[btnTrigg] == 2)
+                return placeFlag(btnTrigg);
             
-            var button = document.querySelectorAll('.btn');
-            let height = Math.floor(Math.random() * 2) + 2, 
-            witdh = Math.floor(Math.random() * 2) + 1 ,pos = 0;
+        if(arrBtn[btnTrigg] % 4 == 0) 
+                return detonateBombs();
+                
+        if(bombs == 0)
+                return winnerSweeper();   
+            
+        var button = $('.btn');
+        let height = Math.floor(Math.random() * 2) + 2, 
+        witdh = Math.floor(Math.random() * 2) + 1 ,pos = 0;
 
-            while(--height) {
-                    for(var i = btnTrigg + pos; i <= btnTrigg + pos + witdh; ++i) {
+        while(--height) {
+                for(var i = btnTrigg + pos; i <= btnTrigg + pos + witdh; ++i) {
                         if(arrBtn[i] % 4 != 0) {
                                 button[i].style.background = "#3a6351";
                                 button[i].disabled = true;
@@ -57,53 +51,58 @@ function makeSteps(btnTrigg) {
                         }
                     }
                     pos += 9;
-            }
-            if(open)
+        }
+            
+        if(open)
                 return generateBombs();
 }
 
 function generateBombs() {
-            open = false, bombs = 0;
-            for(var i = 0; i < 81; ++i) {
-                    if(frq[i] != 1)
+        open = false, bombs = 0;
+        for(var i = 0; i < 81; ++i) {
+                if(frq[i] != 1)
                         arrBtn[i] = Math.floor(Math.random() * 10) + 1;
 
-                    if(arrBtn[i] % 4 == 0)
+                if(arrBtn[i] % 4 == 0)
                         ++bombs;
-            }
+        }
 }
 
 function placeFlag(btnTrigg) {           
-            var button = document.querySelectorAll('.btn');
-            
-            if(frq[btnTrigg] != 2) {    
-                    button[btnTrigg].style.background = "#185adb";
-                    if(arrBtn[btnTrigg] % 4 == 0) {
+        var button = $('.btn');
+        if(frq[btnTrigg] != 2) {    
+                button[btnTrigg].style.background = "#185adb";
+                if(arrBtn[btnTrigg] % 4 == 0) {
                         --bombs;
                         button[btnTrigg].disabled = true;
-                    }
+                }
                     frq[btnTrigg] = 2;
-            } else {
-                    button[btnTrigg].style.background = "#faf1e6";
-                    frq[btnTrigg] = 0;
-            }
+        } else {
+                button[btnTrigg].style.background = "#faf1e6";
+                frq[btnTrigg] = 0;
+        }
 }
 
 function detonateBombs() {           
-            document.getElementById('title').innerHTML = "&#128128;";
-            var button = document.querySelectorAll('.btn');
+        document.getElementById('title').innerHTML = "&#128128;";
+        var button = $('.btn');
 
-            for(var i = 0; i < 81; ++i) {
+        for(var i = 0; i < 81; ++i) {
                 if(arrBtn[i] % 4 == 0) 
                     button[i].style.background = "#962d2d";
+               
                 button[i].disabled = true;
-            }
+        }
 }
 
 function winnerSweeper() {
-            document.getElementById('title').innerHTML = "&#129351;";
-            var button = document.querySelectorAll('.btn');
+        document.getElementById('title').innerHTML = "&#129351;";
+        var button = $('.btn');
 
-            for(var i = 0; i < 81; ++i) 
-                    button[i].disabled = true;
+        for(var i = 0; i < 81; ++i) {
+                if(arrBtn[i] % 4 != 0)
+                     button[i].style.background = "#3a6351"; 
+                
+                button[i].disabled = true;
+        }
 }
